546c344de992ff056104df4a55093e2d6bb2edbd
bemur3 <emuraka@emory.edu>
2025-01-31 14:14:12 -0500

    f

--- submission1/analysis/hwk1_analysis.R
+++ submission1/analysis/hwk1_analysis.R
@@ -2,8 +2,8 @@ if(!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, readxl, data.table, gdata, scales)
 
 #Read data and set workspace for knitr
-full_ma_data <- readRDS("data/output/ma_data_2015.rds")
-contract_service_area <- readRDS("data/output/service_area.rds")
+full_ma_data <- readRDS("/Users/ethanmurakami/Documents/GitHub/Homework-1/data/output/ma_data_2015.rds")
+contract_service_area <- readRDS("/Users/ethanmurakami/Documents/GitHub/Homework-1/data/output/service_area.rds")
 
 #objects for markdown
 plan_type_table <- full_ma_data %>% group_by(plan_type) %>% count() %>% arrange(-n)
@@ -16,12 +16,18 @@ filter(snp == "No" & eghp == "No" &
 
 plan_type_year2 <- final_plans %>% group_by(plan_type) %>% count() %>% arrange(-n) 
 
-plan_type_enroll <- final_plans %>% group_by(plan_type) %>% summarise(n=n(), enrollment=mean(enrollment, na_rm=TRUE)) %>% arrange(-n)
+plan_type_enroll <- plan_type_enroll <- final_plans %>%
+  group_by(plan_type) %>%
+  summarise(
+    Count = n(),
+    Avg_Enrollments = mean(enrollment, na_rm = TRUE)
+  ) %>%
+  arrange(-Count)
 
 final_data <- final_plans %>%
   inner_join(contract_service_area %>%
     select(contractid, fips, year),
-    by = c("contractid" = "fips", "year")) %>%
+    by = c("contractid", "fips", "year")) %>%
   filter(!is.na(enrollment))
 
 rm(list=c("full_ma_data", "contract_service_area", "final_data"))
